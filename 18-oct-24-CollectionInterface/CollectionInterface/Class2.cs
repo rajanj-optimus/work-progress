3aad6ffa1f017835f0400bcbe748cb46d91a86c7
Rajan Kumar Jha <rajan.jha@optimusinfo.com>
2024-10-21 18:46:10 +0530

    Add files via upload

---
+++ 18-oct-24-CollectionInterface/CollectionInterface/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace CustomCollection
+{
+    public class IncrementByTwoCollection<T> :IEnumerable<T>
+    {
+        private List<T> data = new List<T>();
+
+        public void Push(T Val)
+        {
+            data.Add(Val);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < data.Count; i += 2)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+}
---
+++ 18-oct-24-CollectionInterface/CollectionInterface/Program.cs
@@ -0,0 +1,53 @@
+﻿using CustomCollection;
+
+namespace CollectionInterface
+{
+    class A
+    {
+
+        public int a;
+        static  A()
+        {
+
+            Console.WriteLine("static construc");
+
+        }
+      
+        public static void disp()
+        {
+            Console.WriteLine("hi");
+        }
+        public void displ(string s) { Console.WriteLine("nfr"); }
+    }
+    class B : A
+    {
+
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            //IncrementByTwoCollection<int> temp = new IncrementByTwoCollection<int>();
+            ////int num = new int();
+            //temp.Push(1);
+            //temp.Push(2);
+            //temp.Push(3);
+            //temp.Push(4);
+            //temp.Push(5);
+            //foreach (int i in temp)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //B b= new B();
+            //b.disp();
+            //A a= new A();
+            //a.disp();
+            //A.disp();
+            A a = new A();
+            A aa = new A();
+            //a.disp();
+            //B.disp();
+            //A.disp();
+        }
+    }
+}
